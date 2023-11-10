99c6f08383c59b68dbfa22f30d8a999540733238
Danmendua <danmendua@gmail.com>
2023-11-10 16:42:16 -0300

    test new index.htmls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+app.use(cors());
+app.get('/favicon.ico', (req, res) => res.status(204));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const router = require('./router');
 
 app.use(router);
